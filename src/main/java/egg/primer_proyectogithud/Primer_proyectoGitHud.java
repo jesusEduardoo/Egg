a11336c7729559bfd32bd65b8cbc46d775ba7399
Eduardo <eduardo.ninofuentes@gmail.com>
2023-03-29 16:27:19 -0300

    segundo commit

--- src/main/java/egg/primer_proyectogithud/Primer_proyectoGitHud.java
+++ src/main/java/egg/primer_proyectogithud/Primer_proyectoGitHud.java
@@ -7,5 +7,6 @@ public class Primer_proyectoGitHud {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Mi nombre es jesus");
+        System.out.println("aprendiendo con git");
     }
 }
