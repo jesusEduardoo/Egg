8c7d99ebe3bb68dfb54ac4fd3d3996818e3e7793
Eduardo <eduardo.ninofuentes@gmail.com>
2023-03-29 16:16:12 -0300

    primer commit

---
+++ src/main/java/egg/primer_proyectogithud/Primer_proyectoGitHud.java
@@ -0,0 +1,11 @@
+
+package egg.primer_proyectogithud;
+
+
+public class Primer_proyectoGitHud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mi nombre es jesus");
+    }
+}
